8bcf737e7c9b15031b817321a9e0e349d5e47d5d
SilviaChavez <syl_peke14@hotmail.com>
2023-06-13 11:29:23 -0500

    setting up client
    To connect to this server from your client, use "localhost" as the IP address

--- play.js
+++ play.js
@@ -1,17 +1,6 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port:  50541,// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
+
+setupInput(connect());
