2482ef89b422901d87f301cfbcf29c4ee424959c
SilviaChavez <syl_peke14@hotmail.com>
2023-06-14 12:22:38 -0500

    Last Update input.js
    Our play.js file is handling all the logic for setting up and handling user input. Just like with the connection module, we can create an input module which will focus on managing the user input.

--- input.js
+++ input.js
@@ -1,33 +1,36 @@
+// In the input module, create a variable in the outer-most scope called connection, which can default to undefined.
 let connection;
 
-
+// Add the function setupInput to play.js. Copy and paste the following code:
 const setupInput = function(conn) {
-  connection = conn;
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput)
-  return stdin;
-}
+    connection = conn;
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    //stdin.on("data", handleUserInput);
+    stdin.resume();
+    
+    
+    //Create a function called handleUserInput and register it as the "data" callback handler for stdin.
+    const handleUserInput = function(data) {
+      if (data === '\u0003') {
+        process.exit();
+      } else if (data === 'w') {
+        connection.write('Move: up');
+      } else if ( data === 'a') {
+        connection.write('Move: left')
+      } else if ( data === 's') {
+        connection.write('Move: down')
+      } else if ( data === 'd') {
+        connection.write('Move: right')
+      } 
 
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  } else if (data === 'w') {
-    connection.write('Move: up');
-  } else if ( data === 'a') {
-    connection.write('Move: left')
-  } else if ( data === 's') {
-    connection.write('Move: down')
-  } else if ( data === 'd') {
-    connection.write('Move: right')
-  } else if ( data === 'm') {
-    connection.write('Say: hello!!! am Done finally')
-  }
-}
-
-
-module.exports = {
-  setupInput
-}
+    };
+    stdin.on('data', (data) => {
+      handleUserInput(data);
+    });
+    return stdin;
+  
+  };
+  
+  module.exports = { setupInput };
