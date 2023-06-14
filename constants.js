28c5848cdc6c7d0a95d227a231a8d336826b6f50
SilviaChavez <syl_peke14@hotmail.com>
2023-06-14 12:21:28 -0500

    Last Update constants.js
    Create two constants: IP and PORT. Assign them the correct values.

--- constants.js
+++ constants.js
@@ -1,8 +1,9 @@
-const IP = '135.23.222.131';
-const PORT = 50542;
+//Create two constants: IP and PORT. Assign them the correct values.
+const IP = 'localhost';
+const PORT = 50541;
 
 
 module.exports = {
-  IP,
-  PORT
-}
+    IP,
+    PORT,
+  };
